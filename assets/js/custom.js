d5d9a3dea41a689dcb347d76e6c093badea81734
Azhar Haikal Anwar <azharlolypop@gmail.com>
2024-05-27 21:50:18 +0700

    [BlogPage] : Create design category blog and view blog

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,12 @@
+function toggleSearchInput() {
+  var input = document.querySelector('.input-search');
+  if (input.style.display === 'none') {
+    input.style.display = 'inline-block';
+  } else {
+    input.style.display = 'none';
+  }
+}
+
 document.addEventListener('scroll', function() {
   const parallaxImages = document.querySelectorAll('.parallax-img');
   parallaxImages.forEach(img => {
